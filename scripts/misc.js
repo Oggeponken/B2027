880e91255ff636aff3da9e290e158a269bfcd04f
Oskar Hamidi (Consultant) <oskar.hamidi@consultant.volvo.com>
2026-09-21 15:25:10 +0200

    daily commit

--- scripts/misc.js
+++ scripts/misc.js
@@ -1,9 +1,26 @@
-const containers = document.querySelectorAll(".agenda .day-container");
+const containers=document.querySelectorAll(".agenda .day-container");
 
-containers.forEach(function(container) {
-    container.addEventListener("click", function() {
-        container.classList.toggle("expanded");
-    });
+containers.forEach(function(container,index){
+container.addEventListener("click",function(){
+container.classList.toggle("expanded");
+});
+
+const leftArrow=container.querySelector(".arrow.left");
+const rightArrow=container.querySelector(".arrow.right");
+
+leftArrow.addEventListener("click",function(event){
+event.stopPropagation();
+container.classList.remove("expanded");
+const previousIndex=(index-1+containers.length)%containers.length;
+containers[previousIndex].classList.add("expanded");
+});
+
+rightArrow.addEventListener("click",function(event){
+event.stopPropagation();
+container.classList.remove("expanded");
+const nextIndex=(index+1)%containers.length;
+containers[nextIndex].classList.add("expanded");
+});
 });
 
 
@@ -28,3 +45,7 @@ menuLinks.forEach(link => {
         }, 600);
     });
 });
+
+
+
+
